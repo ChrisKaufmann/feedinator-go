872fbcd3134d7248367933de59af1f1663786699
Chris Kaufmann <github@chriskaufmann.com>
2013-04-10 14:57:56 -0500

    moved more functions off to auth and aggregator

---
+++ aggregator.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"html/template"
+	"strconv"
+)
+
+type Category struct {
+	Name        string
+	Description string
+	UserName    string
+	ID          int
+	Unread      int
+	Evenodd     string
+	Class       string
+}
+type Feed struct {
+	ID             int
+	Title          string
+	UserName       string
+	Unread         int
+	Evenodd        string
+	Class          string
+	Url            string
+	LastUpdated    string
+	Public         string
+	Expirey        string
+	CategoryID     int
+	ViewMode       string
+	AutoscrollPX   int
+	Exclude        string
+	ErrorString    string
+	ViewModeSelect template.HTML
+	CategorySelect template.HTML
+}
+type Entry struct {
+	ID       int
+	Evenodd  string
+	Title    string
+	Link     string
+	Date     string
+	FeedName string
+	ViewMode string
+	Marked   string
+	MarkSet  string
+	FeedID   int
+	Content  template.HTML
+	Unread   bool
+}
+
+func getCat(id string) Category {
+	var cat Category
+	err := stmtGetCat.QueryRow(id).Scan(&cat.Name, &cat.UserName, &cat.Description, &cat.ID)
+	if err != nil {
+		err.Error()
+	}
+	return cat
+}
+func getCategoryFeeds(id string) []Feed {
+	var allFeeds []Feed
+	rows, err := stmtGetCatFeeds.Query(id)
+	if err != nil {
+		err.Error()
+		return allFeeds
+	}
+	for rows.Next() {
+		var id string
+		rows.Scan(&id)
+		allFeeds = append(allFeeds, getFeed(id))
+	}
+	return allFeeds
+}
+func getCategories() []Category {
+	var allCats []Category
+	rows, err := stmtGetCats.Query(userName)
+	if err != nil {
+		err.Error()
+		return allCats
+	}
+	for rows.Next() {
+		var cat Category
+		rows.Scan(&cat.Name, &cat.UserName, &cat.Description, &cat.ID)
+		cat.Unread = unreadCategoryCount(cat.ID)
+		if cat.Unread > 0 {
+			cat.Class = "oddUnread"
+		} else {
+			cat.Class = "odd"
+		}
+		allCats = append(allCats, cat)
+	}
+	return allCats
+}
+func getFeeds() []Feed {
+	var allFeeds []Feed
+	rows, err := stmtGetFeeds.Query(userName)
+	if err != nil {
+		err.Error()
+		return allFeeds
+	}
+	for rows.Next() {
+		var feed Feed
+		rows.Scan(&feed.ID, &feed.Title, &feed.Url, &feed.LastUpdated, &feed.UserName, &feed.Public, &feed.Expirey, &feed.CategoryID, &feed.ViewMode, &feed.AutoscrollPX, &feed.Exclude, &feed.ErrorString)
+		feed.Unread = unreadFeedCount(feed.ID)
+		if feed.Unread > 0 {
+			feed.Class = "oddUnread"
+		} else {
+			feed.Class = "odd"
+		}
+		allFeeds = append(allFeeds, feed)
+	}
+	return allFeeds
+}
+func unreadCategoryCount(id int) int {
+	var count int
+	err := stmtCatUnread.QueryRow(id).Scan(&count)
+	if err != nil {
+		err.Error()
+	}
+	return count
+}
+func unreadFeedCount(id int) int {
+	var count int
+	err := stmtFeedUnread.QueryRow(id).Scan(&count)
+	if err != nil {
+		err.Error()
+	}
+	return count
+}
+func getFeed(id string) Feed {
+	var feed Feed
+	err := stmtGetFeed.QueryRow(id).Scan(&feed.ID, &feed.Title, &feed.Url, &feed.LastUpdated, &feed.UserName, &feed.Public, &feed.Expirey, &feed.CategoryID, &feed.ViewMode, &feed.AutoscrollPX, &feed.Exclude, &feed.ErrorString)
+	if err != nil {
+		err.Error()
+	}
+	feed.Unread = unreadFeedCount(feed.ID)
+	if feed.Unread > 0 {
+		feed.Class = "oddUnread"
+	} else {
+		feed.Class = "odd"
+	}
+	if feed.Title == "" {
+		feed.Title = "--untitled--"
+	}
+	return feed
+}
+func getEntry(id string) Entry {
+	//id,title,link,updated,feed_id,marked,content,unread
+	var e Entry
+	var c string
+	err := stmtGetEntry.QueryRow(id).Scan(&e.ID, &e.Title, &e.Link, &e.Date, &e.FeedID, &e.Marked, &c, &e.Unread)
+	if err != nil {
+		err.Error()
+	}
+	if e.Marked == "1" {
+		e.MarkSet = "set"
+	} else {
+		e.MarkSet = "unset"
+	}
+	f := getFeed(strconv.Itoa(e.FeedID))
+	e.Content = template.HTML(unescape(c))
+	e.Title = unescape(e.Title)
+	e.FeedName = f.Title
+	e.ViewMode = f.ViewMode
+	return e
+}
+func markEntry(id string, m string) string {
+	var ret string
+	switch m {
+	case "read":
+		stmtUpdateReadEntry.Exec("0", id)
+	case "unread":
+		stmtUpdateReadEntry.Exec("1", id)
+	case "marked":
+		stmtUpdateMarkEntry.Exec("1", id)
+	case "unmarked":
+		stmtUpdateMarkEntry.Exec("0", id)
+	case "togglemarked":
+		e := getEntry(id)
+		stmtUpdateMarkEntry.Exec(toint(e.Marked)^1, id)
+		en := getEntry(id)
+		ret = "<img src='static/mark_" + en.MarkSet + ".png' alt='Set mark' onclick='javascript:toggleMark(" + id + ");'>\n"
+	}
+	return ret
+}
+func toint(s string) int {
+	i, _ := strconv.Atoi(s)
+	return i
+}
---
+++ auth.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"code.google.com/p/goauth2/oauth"
+	"crypto/sha512"
+	"crypto/tls"
+	"encoding/base64"
+	"encoding/json"
+	"github.com/msbranco/goconfig"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+var oauthCfg = &oauth.Config{
+	AuthURL:     "https://accounts.google.com/o/oauth2/auth",
+	TokenURL:    "https://accounts.google.com/o/oauth2/token",
+	RedirectURL: "http://dev.feedinator.com/oauth2callback",
+	Scope:       "https://www.googleapis.com/auth/userinfo.profile",
+	TokenCache:  oauth.CacheFile(cachefile),
+}
+
+func init() {
+
+	c, err := goconfig.ReadConfigFile("config")
+	if err != nil {
+		panic(err)
+	}
+	oauthCfg.ClientId, err = c.GetString("Google", "ClientId")
+	if err != nil {
+		panic(err)
+	}
+	oauthCfg.ClientSecret, err = c.GetString("Google", "ClientSecret")
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Start the authorization process
+func handleAuthorize(w http.ResponseWriter, r *http.Request) {
+	print("In handleauth\n")
+	//Get the Google URL which shows the Authentication page to the user
+	url := oauthCfg.AuthCodeURL("")
+
+	//redirect user to that page
+	http.Redirect(w, r, url, http.StatusFound)
+}
+
+// Function that handles the callback from the Google server
+func handleOAuth2Callback(w http.ResponseWriter, r *http.Request) {
+	//Get the code from the response
+	code := r.FormValue("code")
+
+	t := &oauth.Transport{oauth.Config: oauthCfg}
+
+	// Exchange the received code for a token
+	tok, _ := t.Exchange(code)
+	{
+		tokenCache := oauth.CacheFile(cachefile)
+
+		err := tokenCache.PutToken(tok)
+		if err != nil {
+			log.Fatal("Cache write:", err)
+		}
+		log.Printf("Token is cached in %v\n", tokenCache)
+	}
+
+	// Skip TLS Verify
+	t.Transport = &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	// Make the request.
+	req, err := t.Client().Get(profileInfoURL)
+	if err != nil {
+		log.Fatal("Request Error:", err)
+	}
+	defer req.Body.Close()
+	body, _ := ioutil.ReadAll(req.Body)
+	log.Println(string(body))
+	//body.id is the google id to use
+	//set a cookie with the id, and random hash. then save the id/hash pair to db for lookup
+	var f interface{}
+	err = json.Unmarshal(body, &f)
+	m := f.(map[string]interface{})
+	print(m["id"].(string))
+	if err != nil {
+		panic(err.Error())
+	}
+	var authString = randomString(64)
+	_, err = stmtCookieIns.Exec(m["id"], hash(authString))
+
+	if err != nil {
+		panic(err.Error())
+	}
+	//set the cookie
+	cookie := http.Cookie{Name: cookieName, Value: authString}
+	http.SetCookie(w, &cookie)
+	http.Redirect(w, r, "/main", http.StatusFound)
+}
+func loggedIn(w http.ResponseWriter, r *http.Request) bool {
+	cookie, err := r.Cookie(cookieName)
+	if err != nil {
+		//just means that the cookie doesn't exist or we couldn't read it
+		return false
+	}
+	tok := cookie.Value
+	tokHash := hash(tok)
+	var userId string
+	err = stmtGet.QueryRow(tokHash).Scan(&userId) // WHERE number = 13
+	if err != nil {
+		return false //probably no rows in result set
+	}
+
+	if userId != "" {
+		userName = userId
+		return true
+	} else {
+		return false
+	}
+	return false
+}
+func randomString(l int) string {
+	rand.Seed(time.Now().UTC().UnixNano())
+	bytes := make([]byte, l)
+	for i := 0; i < l; i++ {
+		bytes[i] = byte(randInt(65, 90))
+	}
+	return string(bytes)
+}
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
+func hash(s string) string {
+	h := sha512.New()
+	h.Write([]byte(s))
+	return base64.URLEncoding.EncodeToString(h.Sum(nil))
+}
