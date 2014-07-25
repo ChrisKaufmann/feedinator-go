554f3294c465e9d2234bb909ebc70821e1465760
Chris Kaufmann <github@chriskaufmann.com>
2014-07-25 10:10:03 -0500

    updated build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-echo "Building feedinator\n"
-go build -o feedinator feedinator main.go db.go auth.go util.go feed.go category.go entry.go
+echo "Building feedinator"
+sh -c 'go build -o feedinator main.go db.go auth.go util.go feed.go category.go entry.go'
 
-echo "Building updater\n"
-go build -o updater update.go db.go auth.go util.go feed.go category.go entry.go
+echo "Building updater"
+sh -c 'go build -o updater update.go db.go auth.go util.go feed.go category.go entry.go'
