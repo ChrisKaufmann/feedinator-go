14846c10a574f44ae9b11382398110c45477ccce
Chris K <ckaufmann@connamara.com>
2016-04-20 13:50:38 -0500

    Very minor change in update feeds

--- update/update_feeds_thready.pl
+++ update/update_feeds_thready.pl
@@ -12,12 +12,7 @@ read_config("../config", %config) or die("Couldn't read config file: $!");
 
 use Data::Dumper;
 print Dumper \%config;
-
-my $db_user	=$config{'DB'}{'user'};
-my $db_pass	=$config{'DB'}{'pass'};
-my $db_host	=$config{'DB'}{'host'}?$config{'DB'}{'host'}:'localhost';
-my $db_db	=$config{'DB'}{'db'};
-my $dbh=DBI->connect("DBI:mysql:$db_db:$db_host","$db_user","$db_pass");
+my $dbh = get_dbh(%config);
 
 my $sql=qq{select id  from ttrss_feeds where 1};
 
@@ -34,3 +29,18 @@ threadeach my $id(shuffle(@all_ids))
 	system("perl update_feeds.pl feed_id=$id");
 }
 print scalar(@all_ids)." feeds updated\n";
+
+
+
+sub get_dbh
+{
+	my %c=shift || die ("No config passed to get_dbh");
+	my $db_user =$config{'DB'}{'user'};
+	my $db_pass =$config{'DB'}{'pass'};
+	my $db_host =$config{'DB'}{'host'}?$config{'DB'}{'host'}:'localhost';
+	my $db_db =$config{'DB'}{'db'};
+
+	my $dbh=DBI->connect("DBI:mysql:$db_db:$db_host","$db_user","$db_pass")
+		|| die("Couldn't connect to db: $!");
+	return $dbh;
+}
