4a7c7adf82db30ed382bed22175e24cce5d6cd26
Chris K <ckaufmann@connamara.com>
2019-07-29 18:31:12 -0500

    Trying to get rid of enter key refreshing the whole page

--- static/include.js
+++ static/include.js
@@ -31,6 +31,19 @@ $(window).keydown(function (e) {
 		}
 	});
 
+$(function() {
+    $('form').each(function() {
+        $(this).find('input').keypress(function(e) {
+            // Enter pressed?
+            if(e.which == 10 || e.which == 13) {
+                this.form.submit();
+            }
+        });
+
+        $(this).find('input[type=submit]').hide();
+    });
+});
+
 function showPreviousEntry(id) {
 	vals = PrevNextTable(id);
 	if(vals==null){return;}
